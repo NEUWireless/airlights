a6835e8c3aec985b80278467c4259465dbf0cd98
Anubhav Sharma <sharma.an@husky.neu.edu>
2019-11-19 17:58:49 -0500

    Added color picker

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from flask import Flask, render_template, flash
 from flask_bootstrap import Bootstrap
+from flask_colorpicker import colorpicker
 import logging
 from forms import ColorNameForm
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'super-secret-key-neu'
 Bootstrap(app)
+colorpicker(app)
 
 logging.basicConfig(level=logging.DEBUG)
 
@@ -16,6 +19,7 @@ def index():
     if color_name_form.validate_on_submit():
         flash('Color String POST: {}'.format(color_name_form.color_name))
         logging.debug("Color entered: %s", color_name_form.color_name)
+    # logging.debug("RGB: %s", request.form)
     return render_template("index.html", form=color_name_form)
 
 
